82f0d3fb0489e73f9fca735cdff902c16cb8aa42
v.khremuchkov <mineexmail@gmail.com>
2024-10-29 19:59:50 -0400

    init rust project

---
+++ Solana/crypto-ticket/programs/crypto-ticket/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("EMGePWFB5TKj6hDYX1q6tfB6PJNhRLigBVtS7ZNyW5zf");
+
+#[program]
+pub mod crypto_ticket {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
